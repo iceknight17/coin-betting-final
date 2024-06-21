c4d3c496afeef4d8b8cf658cfcff0ede6c49b464
iceknight17 <iceknight9717@gmail.com>
2024-06-21 10:36:51 -0400

    first_test

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import WebApp from '@twa-dev/sdk'
 import App from './App.tsx'
 import './index.css'
+
+WebApp.ready();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
